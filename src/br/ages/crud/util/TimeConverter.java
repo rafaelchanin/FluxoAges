5a6fd722df6dff55438d4a2475432ef8fc520d5d
André Botelho <andrebotelho@live.com>
2017-04-05 14:15:35 -0300

    #81 Corrigido bug referente as porcentagens.

--- src/br/ages/crud/util/TimeConverter.java
+++ src/br/ages/crud/util/TimeConverter.java
@@ -1,5 +1,7 @@
 package br.ages.crud.util;
 
+import java.text.DecimalFormat;
+
 public class TimeConverter {
 	public static String ConvertMinuteToHours (int total) {
 		String minutos = String.valueOf(total%60);
@@ -11,6 +13,17 @@ public class TimeConverter {
 		
 		horarioFormatado = horas+ ":"+ minutos;
 		return horarioFormatado;
+	}
+	
+	public static String ConvertMinuteToHours (float total) {
+		DecimalFormat df = new DecimalFormat("0.00");
+		float horas = total/60;
+		return df.format(horas);
+	}
+	
+	public static String ConvertPorcentagemToString (float total) {
+		DecimalFormat df = new DecimalFormat("0.00");
+		return df.format(total);
 	}
 }
 
