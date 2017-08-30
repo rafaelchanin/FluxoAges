cd2d5dbb4f02c5c6182dd44ddfa8914bba98e963
Projetos <ages@pucrs.br>
2017-08-30 19:02:10 -0300

    Update Util.java

--- src/br/ages/crud/util/Util.java
+++ src/br/ages/crud/util/Util.java
@@ -26,18 +26,18 @@ public class Util {
 	}
 
 	public static boolean isCPF(String CPF) {
-		// considera-se erro CPF's formados por uma seqüência de números iguais
+		// considera-se erro CPF's formados por uma seqncia de nmeros iguais
 		if (CPF.equals("00000000000") || CPF.equals("11111111111") || CPF.equals("22222222222") || CPF.equals("33333333333") || CPF.equals("44444444444") || CPF.equals("55555555555")
 				|| CPF.equals("66666666666") || CPF.equals("77777777777") || CPF.equals("88888888888") || CPF.equals("99999999999") || (CPF.length() != 11))
 			return (false);
 		char dig10, dig11;
 		int sm, i, r, num, peso;
-		// "try" - protege o código para eventuais erros de conversão de tipo (int)
+		// "try" - protege o cdigo para eventuais erros de converso de tipo (int)
 		try { // Calculo do 1o. Digito Verificador
 			sm = 0;
 			peso = 10;
 			for (i = 0; i < 9; i++) {
-				// converte o i-ésimo caractere do CPF em um numero:
+				// converte o i-simo caractere do CPF em um numero:
 				// por exemplo, transforma o caractere '0' no inteiro 0
 				// (48 eh a posicao de '0' na tabela ASCII)
 				num = (int) (CPF.charAt(i) - 48);
@@ -49,7 +49,7 @@ public class Util {
 				dig10 = '0';
 			else
 				dig10 = (char) (r + 48);
-			// converte no respectivo caractere numérico
+			// converte no respectivo caractere numrico
 			// Calculo do 2o. Digito Verificador
 			sm = 0;
 			peso = 11;
@@ -63,7 +63,7 @@ public class Util {
 				dig11 = '0';
 			else
 				dig11 = (char) (r + 48);
-			// Verifica se os dígitos calculados conferem com os dígitos informados.
+			// Verifica se os dgitos calculados conferem com os dgitos informados.
 			if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
 				return (true);
 			else
@@ -163,7 +163,7 @@ public class Util {
 		if (semestre == 1)
 			temp = "01"+"/"+"01"+"/"+ano;
 		else
-			temp = "15"+"/"+"07"+"/"+ano;
+			temp = "03"+"/"+"08"+"/"+ano;
 		Date dSemestre = new SimpleDateFormat("dd/MM/yyyy").parse(temp);
 		//System.out.println(semestre);
 		return dSemestre;
