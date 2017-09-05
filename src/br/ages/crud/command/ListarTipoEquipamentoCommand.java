382d26847b0ee035e17e300c491dc651007771b5
guilherme.deconto <guilherme.deconto@acad.pucrs.br>
2017-09-05 16:26:26 -0300

    classe listartipoequipamentocommand implementada

---
+++ src/br/ages/crud/command/ListarTipoEquipamentoCommand.java
@@ -0,0 +1,29 @@
+package br.ages.crud.command;
+
+import br.ages.crud.bo.TipoEquipamentoBO;
+import br.ages.crud.exception.NegocioException;
+import br.ages.crud.model.TipoEquipamento;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ListarTipoEquipamentoCommand {
+    private String proxima;
+    private TipoEquipamentoBO tipoequipamentoBO;
+
+    public String execute(HttpServletRequest request) throws SQLException {
+        this.tipoequipamentoBO = new TipoEquipamentoBO();
+        proxima = "listaequipamento/listaTiposEquipamentos.jsp";
+
+        try {
+            ArrayList<TipoEquipamento> listaTipoEquipamentos = tipoequipamentoBO.listarEquipamentos();
+            request.setAttribute("listaTiposEquipamentos", listaTipoEquipamentos);
+        } catch (NegocioException e) {
+            e.printStackTrace();
+            request.setAttribute("msgErro", e.getMessage());
+        }
+
+        return proxima;
+    }
+}
