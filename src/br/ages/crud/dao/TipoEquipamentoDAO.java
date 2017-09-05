c6a07e1a0be79192c748707f2774a5036ae68cb7
guilherme.deconto <guilherme.deconto@acad.pucrs.br>
2017-09-05 16:09:49 -0300

    add .

--- src/br/ages/crud/dao/TipoEquipamentoDAO.java
+++ src/br/ages/crud/dao/TipoEquipamentoDAO.java
@@ -1,4 +1,46 @@
 package br.ages.crud.dao;
 
+import br.ages.crud.exception.PersistenciaException;
+import br.ages.crud.model.TipoEquipamento;
+import br.ages.crud.util.ConexaoUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class TipoEquipamentoDAO {
+    private ArrayList<TipoEquipamento> tipoequipamentos;
+
+    public ArrayList<TipoEquipamento> listarTipoEquipamentos() throws PersistenciaException, SQLException {
+        Connection conexao = null;
+        try {
+            conexao = ConexaoUtil.getConexao();
+
+            StringBuilder sql = new StringBuilder();
+            sql.append("SELECT ");
+            sql.append("id_tipo_equipamento, ");
+            sql.append("nome, ");
+            sql.append("FROM tb_tipo_equipamento");
+
+            PreparedStatement statement = conexao.prepareStatement(sql.toString());
+            ResultSet resultset = statement.executeQuery();
+
+            tipoequipamentos = new ArrayList<>();
+            while (resultset.next()) {
+                TipoEquipamento dto = new TipoEquipamento();
+                dto.setId(resultset.getInt("id_tipo_equipamento"));
+                dto.setNome(resultset.getString("nome"));
+
+                tipoequipamentos.add(dto);
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new PersistenciaException(e);
+        } finally {
+            conexao.close();
+        }
+        return tipoequipamentos;
+    }
 }
--- src/br/ages/crud/model/TipoEquipamento.java
+++ src/br/ages/crud/model/TipoEquipamento.java
@@ -1,4 +1,39 @@
 package br.ages.crud.model;
 
 public class TipoEquipamento {
+    private int id;
+   private String nome;
+
+    public TipoEquipamento(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public TipoEquipamento() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoEquipamento{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
 }
